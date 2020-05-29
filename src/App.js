7c27c84e16e0449136078bad5e7f825a6fcd0c98
synditrix <no@YourMom.fios-router.home>
2020-05-29 00:27:13 -0400

    stylized + added multi code functionality+

--- src/App.js
+++ src/App.js
@@ -1,35 +1,64 @@
 import React, { Component } from 'react';
 
 import 'antd/dist/antd.css';
-import { Input, Row, Col } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
+import { Input, Row, Col, Menu, Select, Switch, Button, Layout, Typography } from 'antd';
 
 import Dragon from './Dragon';
 
 const { Search } = Input;
+const { Option } = Select;
+const { Header, Footer, Sider, Content } = Layout;
+const { Text } = Typography;
 
 class App extends Component {
 
 	constructor(props) {
 		super(props);
 		this.setRefreshState = this.setRefreshState.bind(this);
+		this.onClickSpeed = this.onClickSpeed.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+		this.switchMulti = this.switchMulti.bind(this);
 		this.state = {
 			code: sessionStorage.getItem('code') || '',
-			isRefreshing: sessionStorage.getItem('isRefreshing') || false
+			isRefreshing: sessionStorage.getItem('isRefreshing') || false,
+			speed: sessionStorage.getItem('speed') || 250, 
+			codes: JSON.parse(sessionStorage.getItem('codes')) || [],
+			multi: sessionStorage.getItem('multi') || false
 		}
 	};
 
 	componentDidMount() {
 		const truthy = this.state.isRefreshing == "true";
 	 	if (truthy) {
- 	 		setInterval(() => window.location.reload(), 500);
+ 	 		setInterval(() => window.location.reload(), this.state.speed);
  		}
 	}
 
-	setRefreshState(option, code) {
+	setRefreshState(option, code, speed, codes, multi) {
 	    sessionStorage.setItem('isRefreshing', option);
 	    sessionStorage.setItem('code', code);
+	    sessionStorage.setItem('speed', speed);
+	    sessionStorage.setItem('codes', JSON.stringify(codes));
+	    sessionStorage.setItem('multi', multi);
 	    this.setState({isRefreshing: option });
 	   	this.setState({code: code });
+	   	this.setState({speed: speed});
+	   	this.setState({codes: codes });
+	   	this.setState({multi: multi });
+	};
+
+	onClickSpeed = (key) => {
+  		this.setState({speed: key});
+	};
+
+	handleChange(value) {
+		const stringvalue = value.toString();
+		this.setState({codes: stringvalue});
+	};
+
+	switchMulti(checked) {
+  		this.setState({multi: checked});
 	}
 
 	render() {
@@ -37,24 +66,76 @@ class App extends Component {
 	    <div className="App">
 	    	{!this.state.isRefreshing && 
 	    	<div>
-	    	<h3>put your dragon here</h3>
+	    	<Layout>
+	    	<Header>
+	    	<Text style={{fontSize: 'x-large', color:'white'}}>
+	    	dragcave.net auto refresher
+	    	</Text>
+	    	</Header>
+	    	<Content style={{ margin: '0 50px' }}>
+	    	<Row style={{marginTop: '1rem'}}>
+	    		<h3>toggle multi-entry mode</h3>
+	    	</Row>
 	    	<Row>
-	    	<Col xs>
-			<Search
-			  placeholder="Input dragon code"
-			  enterButton="Submit"
-			  size="large"
-			  onSearch={
-			  	value => {
-			  		this.setRefreshState(true, value);
-			  		window.location.reload();
-			  	}
-			  }
-			/>
-			</Col>
-			</Row>
+	    		<Switch onChange={this.switchMulti} />
+	    	</Row>
+	    	<Row style={{marginTop: '1rem'}}>
+	    		<Col xs>
+	    		<h3>put your dragon here</h3>
+	    		</Col>
+	    	</Row>
+	    	{!this.state.multi && <Row>
+		    	<Col xs>
+					<Search
+					  placeholder="input dragon code"
+					  enterButton="submit"
+					  size="large"
+					  onSearch={
+					  	value => {
+					  		this.setRefreshState(true, value, this.state.speed, this.state.codes, false);
+					  		window.location.reload();
+					  	}
+					  }
+					/>
+				</Col>
+			</Row>}
+			{this.state.multi && <Row> 
+	    	 <Select size="large" mode="tags" style={{ width: '30%' }} placeholder="input multiple codes" onChange={this.handleChange}>
+			 </Select>
+			 <Button 
+			 	type="primary" 
+			 	size="large" 
+			 	onClick={
+			 		() => {
+			 			this.setRefreshState(true, "", this.state.speed, this.state.codes, true);
+			 			window.location.reload();
+			 		}
+			 	}>submit</Button>
+	    	</Row>}
+			<Row style={{marginTop: '1rem'}}> 
+	    		<h3>select refresh speed</h3>
+	    	</Row>
+	    	<Row style={{marginBottom: '10rem'}}> 
+	    		<Col sm>
+					<Select size="large" defaultValue={this.state.speed + "ms"} onChange={this.onClickSpeed}>
+						<Option key="10">10ms</Option>
+					    <Option key="20">20ms</Option>
+					    <Option key="30">30ms</Option>
+					    <Option key="50">50ms</Option>
+					    <Option key="100">100ms</Option>
+					    <Option key="250">250ms</Option>
+					    <Option key="500">500ms</Option>
+					    <Option key="1000">1000ms</Option>
+					</Select>
+				</Col>
+	    	</Row>
+	    	</Content>
+	    	<Footer>
+				<Text style={{fontSize: 'xx-small'}}>created by synditrix 2020</Text>
+			</Footer>
+	    	</Layout>
 			</div>}
-			{this.state.isRefreshing && <Dragon code={this.state.code} />}
+			{this.state.isRefreshing && <Dragon code={this.state.code} codes={this.state.codes.toString()} multi={this.state.multi}/>}
 	    </div>
 	  );
 	}
--- src/Dragon.js
+++ src/Dragon.js
@@ -5,9 +5,21 @@ import 'antd/dist/antd.css';
 class Dragon extends Component {
 
 	render() {
+		const stringCast = String(this.props.codes).trim();
+		const codeArray = String(stringCast).split(',');
+		const isMulti = this.props.multi == "true";
+	 	const codesList = codeArray != [] && codeArray.map(i => {
+		  	return (
+		  		<div className="Dragons">
+			    <a href={"https://dragcave.net/view/" + i}><img src={"https://dragcave.net/image/" + i} alt="Adopt one today!"/></a>	    
+			    </div>
+		  	)
+	    });
+
 	  return (
 	    <div className="Dragon">
-	    <a href={"https://dragcave.net/view/" + this.props.code}><img src={"https://dragcave.net/image/" + this.props.code} alt="Adopt one today!"/></a>	    
+	    {!isMulti && <a href={"https://dragcave.net/view/" + this.props.code}><img src={"https://dragcave.net/image/" + this.props.code} alt="Adopt one today!"/></a>}   
+	  	{isMulti && codesList}
 	    </div>
 	  );
 	}
